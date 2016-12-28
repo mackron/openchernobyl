5574a54c617d3ae77ccf3d04ad501a5c3c24fb48
David Reid <mackron@gmail.com>
2016-12-28 16:34:59 +1000

    Whitespace.

--- source/oc/ocEngine/Audio/ocAudioContext.cpp
+++ source/oc/ocEngine/Audio/ocAudioContext.cpp
@@ -4,11 +4,11 @@ OC_INLINE ocResult ocToResultFromDRA(dra_result draresult)
 {
     switch (draresult)
     {
-        case DRA_RESULT_SUCCESS:        return OC_RESULT_SUCCESS;
-        case DRA_RESULT_INVALID_ARGS:   return OC_RESULT_INVALID_ARGS;
-        case DRA_RESULT_OUT_OF_MEMORY:  return OC_RESULT_OUT_OF_MEMORY;
-        case DRA_RESULT_NO_BACKEND:     return OC_RESULT_FAILED_TO_INIT_AUDIO;
-        default:                        return OC_RESULT_UNKNOWN_ERROR;
+        case DRA_RESULT_SUCCESS:       return OC_RESULT_SUCCESS;
+        case DRA_RESULT_INVALID_ARGS:  return OC_RESULT_INVALID_ARGS;
+        case DRA_RESULT_OUT_OF_MEMORY: return OC_RESULT_OUT_OF_MEMORY;
+        case DRA_RESULT_NO_BACKEND:    return OC_RESULT_FAILED_TO_INIT_AUDIO;
+        default:                       return OC_RESULT_UNKNOWN_ERROR;
     }
 }
 
