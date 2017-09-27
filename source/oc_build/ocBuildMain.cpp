745ab7b29d2e5c513aa5bc36c578142b57f95bcb
David Reid <mackron@gmail.com>
2017-09-27 19:44:20 +1000

    Add placeholder source files for oc_build.

---
+++ source/oc_build/ocBuildMain.cpp
@@ -0,0 +1,16 @@
+
+#include "ocBuildShaders.hpp"
+#include "ocBuildPipelines.hpp"
+
+#include "ocBuildShaders.cpp"
+#include "ocBuildPipelines.cpp"
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    
+
+    return 0;
+}
